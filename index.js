56e76f1a9b73ca39ad37820277535e7158389a3a
lynohd <lynohd@hotmail.com>
2022-01-01 17:39:35 +0100

    dasd

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/// <reference path="runeappslib.js">
+/// <reference path="imagedetect.js">
+/// <reference path="https://runeapps.org/apps/alt1/alt1api.html">
+
 if(window.alt1 && alt1.versionint > 1001000)
 {
     alt1.identifyAppUrl("appconfig.json");
@@ -7,4 +11,11 @@ if(window.alt1 && alt1.versionint > 1001000)
 function elid(id)
 {
     return document.getElementById(id);
+}
+
+
+function Setup(){
+    var rs = a1lib.bindfullrs();
+    console.log(rs);
+    a1lib.bindregion(rs);
 }
