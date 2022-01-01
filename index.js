9b8f810eb288b9ac8bf98d41df2fff3e340a7b94
lynohd <lynohd@hotmail.com>
2022-01-01 17:15:17 +0100

    Updated

---
+++ index.js
@@ -0,0 +1,10 @@
+if(window.alt1 && alt1.versionint > 1001000)
+{
+    alt1.identifyAppUrl("appconfig.json");
+}
+
+//i'm crippled without this function
+function elid(id)
+{
+    return document.getElementById(id);
+}
